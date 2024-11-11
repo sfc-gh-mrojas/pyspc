dedef459f2a9463b91b8c33da4bc853ebf013d96
Mauricio Rojas Fernandez <mauricio.rojas@snowflake.com>
2024-11-10 22:50:22 -0600

    Update __init__.py

--- pyspc/sampledata/__init__.py
+++ pyspc/sampledata/__init__.py
@@ -1,39 +1,44 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import os
+from io import StringIO
+import pyspc
+_ROOT = 'pyspc/sampledata/'
 
-_ROOT = os.path.abspath(os.path.dirname(__file__))
+def read_csv(filename):
+    data = pyspc.__loader__.get_data(filename).decode('utf-8')
+    return pd.read_csv(StringIO(data))
 
-pistonrings = pd.read_csv(os.path.join(_ROOT, 'pistonrings.csv')).values
+pistonrings = read_csv(os.path.join(_ROOT, 'pistonrings.csv')).values
+    
+diameter = read_csv(os.path.join(_ROOT, 'diameter.csv')).values
 
-diameter = pd.read_csv(os.path.join(_ROOT, 'diameter.csv')).values
+parts = read_csv(os.path.join(_ROOT, 'parts.csv')).values
+    
+sizes = read_csv(os.path.join(_ROOT, 'sizes.csv')).values
+    
+chemical = read_csv(os.path.join(_ROOT, 'chemical.csv')).values
+    
+newproduct = read_csv(os.path.join(_ROOT, 'newproduct.csv')).values
+    
+waitingTime = read_csv(os.path.join(_ROOT, 'waitingTime.csv')).values
 
-parts = pd.read_csv(os.path.join(_ROOT, 'parts.csv')).values
+sandbox = read_csv(os.path.join(_ROOT, 'sandbox.csv')).values
 
-sizes = pd.read_csv(os.path.join(_ROOT, 'sizes.csv')).values
+canjuice = read_csv(os.path.join(_ROOT, 'canjuice.csv')).values
 
-chemical = pd.read_csv(os.path.join(_ROOT, 'chemical.csv')).values
+circuits = read_csv(os.path.join(_ROOT, 'circuits.csv')).values
 
-newproduct = pd.read_csv(os.path.join(_ROOT, 'newproduct.csv')).values
+inspection = read_csv(os.path.join(_ROOT, 'inspection.csv')).values
 
-waitingTime = pd.read_csv(os.path.join(_ROOT, 'waitingTime.csv')).values
+PaintMaterial = read_csv(os.path.join(_ROOT, 'PaintMaterial.csv')).values
 
-sandbox = pd.read_csv(os.path.join(_ROOT, 'sandbox.csv')).values
+Thickness = read_csv(os.path.join(_ROOT, 'thickness.csv')).values
 
-canjuice = pd.read_csv(os.path.join(_ROOT, 'canjuice.csv')).values
+viscosidade = read_csv(os.path.join(_ROOT, 'Viscosidade.csv')).values
 
-circuits = pd.read_csv(os.path.join(_ROOT, 'circuits.csv')).values
+plastic = read_csv(os.path.join(_ROOT, 'hotelling.csv')).values
 
-inspection = pd.read_csv(os.path.join(_ROOT, 'inspection.csv')).values
+experiment = read_csv(os.path.join(_ROOT, 'experiment.csv')).values
 
-paint = pd.read_csv(os.path.join(_ROOT, 'PaintMaterial.csv')).values
-
-thickness = pd.read_csv(os.path.join(_ROOT, 'thickness.csv')).values
-
-viscosity = pd.read_csv(os.path.join(_ROOT, 'Viscosidade.csv')).values
-
-plastic = pd.read_csv(os.path.join(_ROOT, 'hotelling.csv')).values
-
-experiment = pd.read_csv(os.path.join(_ROOT, 'experiment.csv')).values
-
-mewma_example = pd.read_csv(os.path.join(_ROOT, 'dados.csv')).values
+mewma_example = read_csv(os.path.join(_ROOT, 'dados.csv')).values
